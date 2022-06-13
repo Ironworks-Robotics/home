066cb0941aac2c652cdc81b83f2170d4b3ef6f22
Elorie Bernard-Lacroix <eloriel0310@gmail.com>
2022-06-13 16:27:16 -0400

    New commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
@@ -6,12 +5,3 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
-=======
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-});
->>>>>>> 186389bf732e7a51213e8f060ade527c4ef3f76f
